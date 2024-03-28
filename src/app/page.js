1e5df57d7b378facb590f56f63f663dd3f847210
Luaranet <luaranet08@gmail.com>
2024-03-27 20:09:15 -0400

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
+const url = "https://back-end-ifms-marcelino.vercel.app/campi"
+
 export default async function Home() {
 
-  const resposta = await fetch("https://back-end-ifms-marcelino.vercel.app/campi", {
+  const resposta = await fetch(url, {
     next: {
       revalidate: 1
-    }
+    },
+    cache: "no-cache",
+    method: "GET",
+    headers: {'Content-Type':'application/json'}
   });
 
   const campi = await resposta.json();
